f9740c41c6022fdf6d32550eb8e7a45fa9389053
Sw1ftFox <126611653+Sw1ftFox@users.noreply.github.com>
2023-06-13 10:38:44 +0300

    Add files via upload

---
+++ js/global.js
@@ -0,0 +1,15 @@
+document.querySelector("#contact").onclick = () => {
+    document.querySelector(".modal-contact").style.display = "flex";
+}
+
+document.querySelector(".modal-contact").onclick = () => {
+    document.querySelector(".modal-contact").style.display = "none";
+}
+
+document.querySelector("#adress").onclick = () => {
+    document.querySelector(".modal-adress").style.display = "flex";
+}
+
+document.querySelector(".modal-adress").onclick = () => {
+    document.querySelector(".modal-adress").style.display = "none";
+}
